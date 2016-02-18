3025cf7c2615656917ecf4f9534dd2713323c640
Lex Myers <lex.myers@gmail.com>
2016-02-18 12:51:01 -0800

    documented articlesController.loadByAuthor method

--- class-14-managing-state/lex-ben_h/scripts/articleController.js
+++ class-14-managing-state/lex-ben_h/scripts/articleController.js
@@ -20,7 +20,7 @@
 // callback, articleData saves those results to the "articles" property of
 // the context object. It then fires off the next callback function, which
 // in the call to page() in routes.js, will be "articlesController.index".
-  
+
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -31,6 +31,18 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+
+// Much in the same way as the loadById method, this one selects articles
+// where the author matches the name from the URL. Whenever the URL is of
+// the form "/author/:authorName", the authorName parameter is passed in
+// as a property of the context object and used to query the master SQL
+// table using the Article.findWhere method, after all the "+" characters
+// are replace()d with spaces. That method then executes authorData as a
+// callback, which sets the "articles" property of the context object to
+// the results of the query. "loadByAuthor" then runs next() to execute
+// the next callback, which in this case will again be
+// articlesController.index(). 
+
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
